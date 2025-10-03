2dcda5debf6a6d63056513d40815576ac84260cc
Satya Sri <satyasri@example.com>
2025-10-04 01:52:18 +0530

    backend url updated

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = "https://resume-builder-backend-udt3.onrender.com";
 
 //ROUTES USED FOR FRONTEND 
 export const API_PATHS={
